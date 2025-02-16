75f806778fb466ee862059675456d29d799d8697
Nicolas vauché <hello@nicolasvauche.net>
2025-02-16 12:04:06 +0100

    Secure API calls

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request, HTTPException
 from .schemas import RecommendationRequest, Album
 from .services import recommend_album
+
+load_dotenv()
+API_TOKEN = os.getenv("ML_API_TOKEN")
 
 app = FastAPI()
 
@@ -14,5 +19,15 @@ async def api_home():
 
 
 @app.post("/recommend", response_model=Album)
-async def get_recommendation(request: RecommendationRequest):
+async def get_recommendation(request: RecommendationRequest, req: Request):
+    auth_header = req.headers.get("Authorization")
+
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Missing or invalid token")
+
+    token = auth_header.split("Bearer ")[1]
+
+    if token != API_TOKEN:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     return recommend_album(request)
